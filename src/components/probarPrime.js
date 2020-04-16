2b4f64b6705b9d78cec6e7d4ea8312250fd9ddbf
CBNaS <protocolcbr@gmail.com>
2020-04-15 20:29:49 -0500

    Componentes para cambiar entre paginas

---
+++ src/components/probarPrime.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class probarPrime extends Component {
+    render() {
+        return (
+            <Link to = "/Probar">Probar Prime</Link>
+        )
+    }
+}
